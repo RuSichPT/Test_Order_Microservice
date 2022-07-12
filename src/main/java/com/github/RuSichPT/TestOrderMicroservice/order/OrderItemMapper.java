a8f3068d984ebd95bedc4f8908f2bae97e7fd547
pavel.tokarev <pavel.tokarev@mail.ntc51>
2022-07-12 10:44:02 +0300

    Go to xml. OrderItemMapper is ready

--- src/main/java/com/github/RuSichPT/TestOrderMicroservice/order/OrderItemMapper.java
+++ src/main/java/com/github/RuSichPT/TestOrderMicroservice/order/OrderItemMapper.java
@@ -7,24 +7,12 @@ import java.util.List;
 @Mapper
 public interface OrderItemMapper {
 
-    String insert = "INSERT INTO \"order_item\"(ID, ORDER_ID, ITEM_NAME)" +
-                    "VALUES (NEXTVAL('order_item_seq'), CURRVAL('order_seq'), #{itemName});";
-    String selectByOrderId = "SELECT * FROM \"order_item\" WHERE ORDER_ID = #{order_id}";
-    String updateByOrderId = "UPDATE \"order_item\" " +
-                             "SET ITEM_NAME = #{itemName} " +
-                             "WHERE ORDER_ID = #{order_id}";
-    String deleteByOrderId = "DELETE FROM \"order_item\" WHERE ORDER_ID = #{order_id}";
-
-    @Insert(insert)
     void createOrder(String itemName);
 
-    @Select(selectByOrderId)
     List<OrderItem> getOrderItemByOrderId(int order_id);
 
-    @Update(updateByOrderId)
     void updateByOrderId(int order_id, String itemName);
 
-    @Delete(deleteByOrderId)
     void deleteOrderByOrderId(int order_id);
 
 }
